bf5e8dda8e79e3959aea8c4de64329f2c1be73c5
Yotam loewenbach <yotam.loewenbach@logz.io>
2023-09-19 19:45:04 +0300

    Update instrumentation

--- django-celery/project/gunicorn.conf.py
+++ django-celery/project/gunicorn.conf.py
@@ -31,7 +31,7 @@ from opentelemetry.sdk.trace.export import BatchSpanProcessor
 
 bind = "0.0.0.0:8000"
 
-#os.environ.setdefault("DJANGO_SETTINGS_MODULE", "core.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "core.settings")
 # Sample Worker processes
 workers = 4
 worker_class = "sync"
@@ -47,39 +47,10 @@ access_log_format = (
     '%(h)s %(l)s %(u)s %(t)s "%(r)s" %(s)s %(b)s "%(f)s" "%(a)s"'
 )
 
-
+DjangoInstrumentor().instrument()
 def post_fork(server, worker):
     server.log.info("Worker spawned (pid: %s)", worker.pid)
 
-    resource = Resource.create(
-        attributes={
-            "service.name": "django-celery-vp",
-            # If workers are not distinguished within attributes, traces and
-            # metrics exported from each worker will be indistinguishable. While
-            # not necessarily an issue for traces, it is confusing for almost
-            # all metric types. A built-in way to identify a worker is by PID
-            # but this may lead to high label cardinality. An alternative
-            # workaround and additional discussion are available here:
-            # https://github.com/benoitc/gunicorn/issues/1352
-            "worker": worker.pid,
-        }
-    )
 
-    trace.set_tracer_provider(TracerProvider(resource=resource))
-    # This uses insecure connection for the purpose of example. Please see the
-    # OTLP Exporter documentation for other options.
-    span_processor = BatchSpanProcessor(
-        OTLPSpanExporter(endpoint="http://otelcol:4317", insecure=True)
-    )
-    trace.get_tracer_provider().add_span_processor(span_processor)
 
-    reader = PeriodicExportingMetricReader(
-        OTLPMetricExporter(endpoint="http://otelcol:4317")
-    )
-    metrics.set_meter_provider(
-        MeterProvider(
-            resource=resource,
-            metric_readers=[reader],
-        )
-    )
 
